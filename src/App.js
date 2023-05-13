ba9eb2b6844667dead41de2a1c3822f1f9e0dde4
Brandon Alexander <bjvonalexander@gmail.com>
2023-05-12 19:42:55 -0500

    Video 151 - 161
    1. Project setup
    2. Some Button Theory
    3. Underlying Elements
    4. The Children Prop
    5.Props Design
    6.Validating Props with PropTypes
    7.Install Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div>
+            <div>
+                <Button success rounded outline>Click Me!!</Button>
+            </div>
+            <div>
+                <Button danger outline>Buy Now!</Button>
+            </div>
+            <div>
+                <Button warning>See Deals!</Button>
+            </div>
+            <div>
+                <Button secondary outline>Hide Ads!</Button>
+            </div>
+            <div>
+                <Button secondary rounded>Something!</Button>
+            </div>
+        </div>
+    );
+
+};
 
 export default App;
