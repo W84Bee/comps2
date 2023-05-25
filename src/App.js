b9a6fda6e90859b0a944b537a42164a73863b304
Brandon Alexander <bjvonalexander@gmail.com>
2023-05-25 14:39:41 -0500

    Making Navigation Reusable
    Popular navigation libraries
    - React-Router
    - Wouter
    - React-Location
    - React-Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import Link from "./components/Link";
+
 import Route from "./components/Route";
 import AccordionPage from "./pages/AccordionPage";
 import DropDownPage from "./pages/DropDownPage";
+import SideBar from "./components/SideBar";
+import ButtonPage from "./pages/ButtonPage";
 function App() {
     return (
-        <div>
-            <Link to="/accordion">Go to Accordion</Link>
-            <Link to="/dropdown">Go to Dropdown</Link>
-            <div>
+        <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+            <SideBar />
+            <div className='col-span-5'>
                 <Route path="/accordion">
                     <AccordionPage />
                 </Route>
-                <Route path="/dropdown">
+                <Route path="/">
                     <DropDownPage />
                 </Route>
+                <Route path="/buttons">
+                    <ButtonPage />
+                </Route>
+
             </div>
         </div>
     );
